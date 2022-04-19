07400a65a0784b498e1b9269c53ed6bd9bc03923
cyl <609438328@qq.com>
2022-04-19 12:50:08 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import AutoImport from "unplugin-auto-import/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/my-manage/dist',
+  base:'/my-manage/dist/',
   plugins: [
     vue(),
     AutoImport ({
